27d2eb7aeff7dac152b8d3fdbffd120b7fcf76cb
2YoonJae <leeyoonjae2313@gmail.com>
2026-04-01 11:35:21 +0900

    4주차 - 실습문제

--- quiz9.sh
+++ quiz9.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 touch multiLine.txt
-echo -e "Line1 \n Line2 \n Line3 \n Line4 \n Line5" > multiLine.txt
+echo "Line1 \nLine2 \nLine3 \nLine4 \nLine5 \nLine6" > multiLine.txt
+head -3 multiLine.txt
